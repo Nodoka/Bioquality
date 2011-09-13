bdfc3c13c1ba4e2ce41bb847710b5571ac8ad4fc
Matt Gibb <matt@mattgibb.com>
2011-09-13 01:29:18 +0100

    Script to generate prefecture-species pairs.

---
+++ bin/generate_pref_species.R
@@ -0,0 +1,16 @@
+# load all data
+species_cell <- read.csv('../data/species_cell.csv', row.names = NULL)
+cell_pref    <- read.csv('../data/cell_pref.csv',    row.names = NULL)
+
+# filter for relevant columns
+cell_pref <- cell_pref[,c('X.Pref_en','sampname')]
+species_cell <- species_cell[,c('sampname','species','star')]
+
+# merge dataframes
+species_pref <- merge(species_cell, cell_pref)
+
+# select unique species-prefecture pairs
+species_pref <- unique(species_pref[,c('species','star','X.Pref_en')])
+
+# write results to file
+write.csv(species_pref, file="../data/species_pref.csv", fileEncoding="UTF-8")
