3529771c3e9bc7c426323cb16cb2bedbf86f3bd9
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-02-23 11:49:06 +0000

    Added process to convert results to dataframe.

--- bin/compare_GHI_by_prefecture.R
+++ bin/compare_GHI_by_prefecture.R
@@ -9,7 +9,7 @@ foj  <- read.csv('../data/species_pref_foj.csv', row.names = NULL)
 hori <- sanitise(hori)
 foj  <- sanitise(foj)
 
-# number of species by star and prefecture
+# calculate GHI scores on prefectures by using number of species by star and prefecture
 calculate_score <- function(subgroup) {
   GHI.score(GHI.star_frequencies(subgroup))
 }
@@ -21,18 +21,46 @@ calculate_proportions <- function(subgroup) {
 hori_scores <- tapply(hori$star, hori$pref, calculate_score)
 foj_scores  <- tapply(foj$star,  foj$pref,  calculate_score)
 
-# paired-t test (correaltion analysis as second step) on scores between hori and foj
-# t.test(hori_scores,foj_scores,paired=TRUE)
+
+# merge and transpose scores into one table
+merged_scores <- t(rbind(foj_scores,hori_scores))
+# filter out invalid values (NA) on scores
+valid_scores <- na.omit(merged_scores)
+# convert scores to dataframe
+scores_frame <- data.frame(preflist=row.names(valid_scores), foj_scores=valid_scores[,1], hori_scores=valid_scores[,2], row.names=NULL)
+
+# write results (scores) to file
+write.csv(scores_frame,
+          file="../data/scores.csv",
+          row.names=FALSE,
+          fileEncoding="UTF-8")
 
 
-# write results (scores) to file, to be checked for labels
-write.csv(hori_scores,
-          file="../data/hori_scores.csv",
-          fileEncoding="UTF-8",)
+# scatter plot on scores
+plot(valid_scores[,1],valid_scores[,2])
+# scatter plot onscores in ranked order
+plot(rank(valid_scores[,1]),rank(valid_scores[,2]))
 
-write.csv(foj_scores,
-          file="../data/foj_scores.csv",
-          fileEncoding="UTF-8",)
+# ranked correlation analysis in 3 ways. uncomment to run alternative methods.
+cor(rank(valid_scores[,1]),rank(valid_scores[,2]))
+# cor(valid_scores[,1],valid_scores[,2], method = "spearman")
+# cor(foj_scores,hori_scores, use= "na.or.complete", method = "spearman")
 
+# fit a linear model (linear regression analysis)
+compare_scores <- lm(rank(valid_scores[,2]) ~ rank(valid_scores[,1]))
+# draw a linear model graph with a linear regression line
+abline(compare_scores)
+
+# paired-t test on scores between hori and foj. 
+# This test is probably not good because data are not normally distributed (check histogram).
+t.test(hori_scores,foj_scores,paired=TRUE)
+# histogram of scores
+plot(density(foj_scores,na.rm=TRUE))
+plot(density(hori_scores,na.rm=TRUE))
+
+
+# bar plot
 barplot(rbind(hori_scores, foj_scores), beside=TRUE)
+
+
 
