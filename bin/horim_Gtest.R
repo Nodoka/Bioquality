a0be03d9e03a7a195880cf1d61e845d616077577
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-02-27 10:10:13 +0000

    Added valid star filering function sourced from sanitise_data.

--- bin/horim_Gtest.R
+++ bin/horim_Gtest.R
@@ -1,5 +1,10 @@
+source('../library/sanitise_data.R')
+
 # load data
 horiw <- read.csv('../data/Hori_area_weight.csv', row.names=NULL)
+
+# filter for valid stars
+horiw <- filter_rows_by_valid_star(horiw,'star_geo')
 
 # calculate mean of selected column (count of grid cells of occurrence) grouped by stars
 horic_mgr <- tapply(horiw$horimgrid, horiw$star_geo, mean)
@@ -7,7 +12,7 @@ horic_qgr <- tapply(horiw$quartergrid, horiw$star_geo, mean)
 horic_1gr <- tapply(horiw$X1grid, horiw$star_geo, mean)
 
 # merge resulting mean tables to one
-meanhoric <- rbind(horic_mgr,horic_qgr,horic_1gr)
+meanhori_count <- rbind(horic_mgr,horic_qgr,horic_1gr)
 
 # calculate mean of selected column (total land area of grid cells of occurrence) grouped by stars
 horia_mgr <- tapply(horiw$mgr_totalland, horiw$star_geo, mean)
@@ -15,4 +20,4 @@ horia_qgr <- tapply(horiw$qgr_totalland, horiw$star_geo, mean)
 horia_1gr <- tapply(horiw$X1gr_totalland, horiw$star_geo, mean)
 
 # merge resulting mean tables to one
-meanhoria <- rbind(horia_mgr,horia_qgr,horia_1gr)
+meanhori_area <- rbind(horia_mgr,horia_qgr,horia_1gr)
