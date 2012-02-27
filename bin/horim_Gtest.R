afecf5809f8a9f3a2e2d5b7672e90e58e59b0d1a
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-02-27 11:56:59 +0000

    Added two filter functions: endemics and main island grids - these can be placed in a new sanitise file in library. Added mean calculation.

--- bin/horim_Gtest.R
+++ bin/horim_Gtest.R
@@ -6,13 +6,34 @@ horiw <- read.csv('../data/Hori_area_weight.csv', row.names=NULL)
 # filter for valid stars
 horiw <- filter_rows_by_valid_star(horiw,'star_geo')
 
+# filter species for Japanese endemics
+filter_endemics <- function(hori_data) {
+  # only consider GHI.colours in hori_data
+  hori_data <- subset(hori_data, horim_end %in% 'endemic')
+
+  # remove unnecessary levels in hori_data$horim_end
+  hori_data$horim_end <- factor(hori_data$horim_end, 'endemic')
+  
+  return(hori_data)
+}
+
+# uncomment to run analysis only with endemics
+# horiw <- filter_endemics(horiw)
+
+# filter species recorded in mainisland 
+filter_mainisl <- function(hori_data) {
+  # only consider grids on main islands in hori_data
+  hori_data <- subset(hori_data, mgr_mainisl > 0)  
+  return(hori_data)
+}
+
+# uncomment to run analysis only with grids on main islands
+# horiw <- filter_mainisl(horiw)
+
 # calculate mean of selected column (count of grid cells of occurrence) grouped by stars
 horic_mgr <- tapply(horiw$horimgrid, horiw$star_geo, mean)
 horic_qgr <- tapply(horiw$quartergrid, horiw$star_geo, mean)
 horic_1gr <- tapply(horiw$X1grid, horiw$star_geo, mean)
-
-# merge resulting mean tables to one
-meanhori_count <- rbind(horic_mgr,horic_qgr,horic_1gr)
 
 # calculate mean of selected column (total land area of grid cells of occurrence) grouped by stars
 horia_mgr <- tapply(horiw$mgr_totalland, horiw$star_geo, mean)
@@ -20,4 +41,9 @@ horia_qgr <- tapply(horiw$qgr_totalland, horiw$star_geo, mean)
 horia_1gr <- tapply(horiw$X1gr_totalland, horiw$star_geo, mean)
 
 # merge resulting mean tables to one
+meanhori_count <- rbind(horic_mgr,horic_qgr,horic_1gr)
 meanhori_area <- rbind(horia_mgr,horia_qgr,horia_1gr)
+
+# calculate weights for stars
+weight_count <- meanhori_count[,'GN']/meanhori_count
+weight_area <- meanhori_area[,'GN']/meanhori_area
