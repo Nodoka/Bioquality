84d16a770c276f8665023af3f31b4116b7eaf74a
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-02-27 13:58:07 +0000

    Added valid star filtering function, calculation of weights and modified savePlot.

--- bin/tdwg_Gtest.R
+++ bin/tdwg_Gtest.R
@@ -1,5 +1,10 @@
+source('../library/sanitise_data.R')
+
 # load data
 tdwg <- read.csv('../data/tdwgsp.csv',     row.names = NULL)
+
+# filter for valid stars
+tdwg <- filter_rows_by_valid_star(tdwg,'star_geo')
 
 # calculate mean of selected column grouped by stars
 tdwgc <- tapply(tdwg$tdwgtotals, tdwg$star_geo, mean)
@@ -8,12 +13,13 @@ tdwga <- tapply(tdwg$tdwgareas, tdwg$star_geo, mean)
 # merge resulting mean tables to one
 meantdwg <- rbind(tdwgc,tdwga)
 
+# calculate weights for stars
+weight_tdwg <- meantdwg[,'GN']/meantdwg
+
 # scatter plot
 plot(tdwg$tdwgtotals,tdwg$tdwgareas)
 
 # save plot on display as a file
-     savePlot(filename = paste("Rplot", type, sep="."),
-              type = c("png", "jpeg", "tiff", "bmp"),
-              device = dev.cur())
+     savePlot(filename = "../data/twdg_scatter.tiff", type="tiff")
      
 
