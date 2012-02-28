528ff91b7270967fdb808b7d0adf9634d8e7f9e9
Matt Gibb <matt@mattgibb.com>
2012-02-28 13:43:16 +0000

    Stacked bar plot of populations broken into categories.

--- graphing/bar_stacked.py
+++ graphing/bar_stacked.py
@@ -18,15 +18,29 @@ star_kew      = map(lambda row: row[2], spno_star)
 # check that star names are the same and in the same order
 assert geo_category == star_category
 
-# plot data
-ind = np.arange(4)
-width = 0.35
-p1 = plt.bar(ind, geo_foj, width, color='r')
-p2 = plt.bar(ind, geo_kew, width, color='y')
+# create array of populations
+populations = np.array([geo_foj,geo_kew,star_foj,star_kew])
 
-plt.ylabel('Scores')
-plt.title('')
-plt.xticks(ind+width/2., geo_category )
-plt.legend( (p1[0], p2[0]), ('GEO FOJ', 'GEO Kew') )
+# plot data
+def plot_stacked_bar(results):
+    # extract each column into a variable
+    bk = results[:,0]
+    gd = results[:,1]
+    bu = results[:,2]
+    gn = results[:,3]
+    ind = np.arange(4)
+    width = 0.35
+    p1 = plt.bar(ind, bk, width, color='black')
+    p2 = plt.bar(ind, gd, width, color='yellow', bottom=bk)
+    p3 = plt.bar(ind, bu, width, color='blue'  , bottom=bk + gd)
+    p4 = plt.bar(ind, gn, width, color='green' , bottom=bk + gd + bu)
+    plt.xlabel('Data Source')
+    plt.legend( (p1[0], p2[0], p3[0], p4[0]), geo_category )
+    plt.xticks(ind+width/2., ('GEO FOJ','GEO Kew','Star FOJ','Star Kew') )
+    
+plot_stacked_bar(populations)
+
+plt.ylabel('Number of Taxa')
+plt.title('Number of Taxa Assigned to Stars from 4 Data Sources')
 
 plt.show()
