8562f05d5bac8825e20a3c4a0fab73a4ed49a1f4
Matt Gibb <matt@mattgibb.com>
2012-02-28 14:09:31 +0000

    Swapped order of Star and Geo in script.

--- graphing/bar_stacked.py
+++ graphing/bar_stacked.py
@@ -4,22 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # read files
-spno_geo  = np.genfromtxt('../data/FOJKew_geo_Gtestdata.csv',  delimiter=',', dtype = None, names=True)
 spno_star = np.genfromtxt('../data/FOJKew_star_Gtestdata.csv', delimiter=',', dtype = None, names=True)
+spno_geo  = np.genfromtxt('../data/FOJKew_geo_Gtestdata.csv',  delimiter=',', dtype = None, names=True)
 
 # extract columns
-geo_category  = map(lambda row: row[0], spno_geo )
-geo_foj       = map(lambda row: row[1], spno_geo )
-geo_kew       = map(lambda row: row[2], spno_geo )
 star_category = map(lambda row: row[0], spno_star)
 star_foj      = map(lambda row: row[1], spno_star)
 star_kew      = map(lambda row: row[2], spno_star)
+geo_category  = map(lambda row: row[0], spno_geo )
+geo_foj       = map(lambda row: row[1], spno_geo )
+geo_kew       = map(lambda row: row[2], spno_geo )
 
 # check that star names are the same and in the same order
-assert geo_category == star_category
+assert star_category == geo_category
 
 # create array of populations
-populations = np.array([geo_foj,geo_kew,star_foj,star_kew])
+populations = np.array([star_foj,star_kew,geo_foj,geo_kew])
 
 # plot data
 def plot_stacked_bar(results):
@@ -35,11 +35,10 @@ def plot_stacked_bar(results):
     p3 = plt.bar(ind, bu, width, color='blue'  , bottom=bk + gd)
     p4 = plt.bar(ind, gn, width, color='green' , bottom=bk + gd + bu)
     plt.xlabel('Data Source')
-    plt.legend( (p1[0], p2[0], p3[0], p4[0]), geo_category )
-    plt.xticks(ind+width/2., ('GEO FOJ','GEO Kew','Star FOJ','Star Kew') )
+    plt.legend( (p1[0], p2[0], p3[0], p4[0]), star_category )
+    plt.xticks(ind+width/2., ('Star FOJ','Star Kew','GEO FOJ','GEO Kew') )
     
 plot_stacked_bar(populations)
-
 plt.ylabel('Number of Taxa')
 plt.title('Number of Taxa Assigned to Stars from 4 Data Sources')
 
