b56d0b0c1bece81333ad45570d10fe433dd6a374
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-02-28 10:37:57 +0000

    Corrected some gaps to make scripts easy to read.

--- bin/examine_GHI_by_prefecture_for_star_sensitivity.R
+++ bin/examine_GHI_by_prefecture_for_star_sensitivity.R
@@ -13,8 +13,8 @@ foj <- set_pref_levels(foj)
 # how to avoid redundancy in star* used across this section?
 # foj_star* <- filter_rows_by_valid_star(foj,'star*')
 
-foj_star <- filter_rows_by_valid_star(foj,'star')
-foj_star_geo <- filter_rows_by_valid_star(foj,'star_geo')
+foj_star      <- filter_rows_by_valid_star(foj,'star')
+foj_star_geo  <- filter_rows_by_valid_star(foj,'star_geo')
 foj_star_geou <- filter_rows_by_valid_star(foj,'star_geou')
 foj_star_geod <- filter_rows_by_valid_star(foj,'star_geod')
 foj_star_infa <- filter_rows_by_valid_star(foj,'star_infa')
@@ -26,21 +26,27 @@ foj_star_infs <- filter_rows_by_valid_star(foj,'star_infs')
 # sens_scores <- cbind (foj_scores_star*,,,)
 
 # individual scripts run in R
-foj_scores_star <- tapply(foj_star$star,  foj_star$pref, calculate_score)
-foj_scores_star_geo <- tapply(foj_star_geo$star_geo,  foj_star_geo$pref,  calculate_score)
+foj_scores_star       <- tapply(foj_star$star,  foj_star$pref, calculate_score)
+foj_scores_star_geo   <- tapply(foj_star_geo$star_geo,  foj_star_geo$pref,  calculate_score)
 foj_scores_star_geou  <- tapply(foj_star_geou$star_geou,  foj_star_geou$pref,  calculate_score)
 foj_scores_star_geod  <- tapply(foj_star_geod$star_geod,  foj_star_geod$pref,  calculate_score)
 foj_scores_star_infa  <- tapply(foj_star_infa$star_infa,  foj_star_infa$pref,  calculate_score)
 foj_scores_star_infs  <- tapply(foj_star_infs$star_infs,  foj_star_infs$pref,  calculate_score)
-sens_scores <- cbind(foj_scores_star,foj_scores_star_geo,foj_scores_star_geou,foj_scores_star_geod,foj_scores_star_infa,foj_scores_star_infs)
+sens_scores           <- cbind(foj_scores_star,foj_scores_star_geo,foj_scores_star_geou,foj_scores_star_geod,foj_scores_star_infa,foj_scores_star_infs)
 
 # calculate the differences of each star method from the default
-differences <- sens_scores[,-1] - replicate(5,sens_scores[,1])
+differences      <- sens_scores[,-1] - replicate(5,sens_scores[,1])
 difference_means <- apply(differences,2,mean)
-difference_sds <- apply(differences,2,sd)
+difference_sds   <- apply(differences,2,sd)
 
 # convert scores to dataframe
-sens_scores_frame <- data.frame(preflist=row.names(sens_scores), star_default=sens_scores[,1], star_geo=sens_scores[,2], star_geou=sens_scores[,3],star_geod=sens_scores[,4], star_infa=sens_scores[,5], star_infs=sens_scores[,6], row.names=NULL)
+sens_scores_frame <- data.frame(preflist=row.names(sens_scores), 			     star_default=sens_scores[,1], 
+		     star_geo=sens_scores[,2], 
+                     star_geou=sens_scores[,3],
+                     star_geod=sens_scores[,4], 
+                     star_infa=sens_scores[,5], 
+                     star_infs=sens_scores[,6],
+                     row.names=NULL)
 
 # write results (scores) to file
 write.csv(sens_scores_frame,
