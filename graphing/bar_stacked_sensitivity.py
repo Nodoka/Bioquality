90d88f2dafd9e02a49d17244afc918309b95fc2b
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-03-01 14:09:21 +0000

    Added new bar stacked graphs for different star classification methods.

---
+++ graphing/bar_stacked_sensitivity.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# a stacked bar plot with errorbars
+import numpy as np
+import matplotlib.pyplot as plt
+
+# extract columns of each dataset
+label = np.genfromtxt('../data/star_sensitivity_spnos_data.csv',delimiter=',',dtype=None,skip_header=1,usecols=0)
+
+star_sens = np.genfromtxt('../data/star_sensitivity_spnos_data.csv',delimiter=',',dtype=None,skip_header=1,usecols=range(1,7))
+
+# plot data
+def plot_stacked_bar(results):
+    # extract each column into a variable
+    bk = results[0,:]
+    gd = results[1,:]
+    bu = results[2,:]
+    gn = results[3,:]
+    ind = np.arange(6)
+    width = 0.35
+    p1 = plt.bar(ind, bk, width, color='black')
+    p2 = plt.bar(ind, gd, width, color='yellow', bottom=bk)
+    p3 = plt.bar(ind, bu, width, color='blue'  , bottom=bk + gd)
+    p4 = plt.bar(ind, gn, width, color='green' , bottom=bk + gd + bu)
+    plt.xlabel('Star Classification Method')
+    plt.legend( (p1[0], p2[0], p3[0], p4[0]), label)
+    plt.xticks(ind+width/2., ('Star default','Star GEO','Star GEO-Up','Star GEO-Down','Star INFRA-Auto','Star INFRA-Selective') )
+
+plot_stacked_bar(star_sens)
+plt.ylabel('Number of Taxa')
+plt.title('Number of Taxa Assigned to Stars for 6 Star Classification Methods')
+
+plt.show()
+
+# calculate proportions by normalising populations
+# use float to prevent integer arithmetic (rounding down to zero)
+prop_star_sens = star_sens /sum(star_sens).astype(float)
+
+plot_stacked_bar(prop_star_sens)
+plt.ylabel('Proportion of Taxa')
+plt.title('Proportion of Taxa Assigned to Stars for & Star Classification Methods')
+
+plt.show()
