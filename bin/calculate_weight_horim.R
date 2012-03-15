679a32f12dab77959c2ecd329ce565bdb5e5f453
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-03-15 17:42:06 +0000

    Changed star_geo to star_infs in filtering valid stars.

--- bin/calculate_weight_horim.R
+++ bin/calculate_weight_horim.R
@@ -4,7 +4,7 @@ source('../library/sanitise_data.R')
 horiw <- read.csv('../data/Hori_area_weight.csv', row.names=NULL)
 
 # filter for valid stars
-horiw <- filter_rows_by_valid_star(horiw,'star_geo')
+horiw <- filter_rows_by_valid_star(horiw,'star_infs')
 
 # filter species for Japanese endemics
 filter_endemics <- function(hori_data) {
@@ -34,14 +34,14 @@ filter_mainisl <- function(hori_data) {
 # horiw <- filter_mainisl(horiw)
 
 # calculate mean of selected column (count of grid cells of occurrence) grouped by stars
-horic_mgr <- tapply(horiw$horimgrid, horiw$star_geo, mean)
-horic_qgr <- tapply(horiw$quartergrid, horiw$star_geo, mean)
-horic_1gr <- tapply(horiw$X1grid, horiw$star_geo, mean)
+horic_mgr <- tapply(horiw$horimgrid, horiw$star_infs, mean)
+horic_qgr <- tapply(horiw$quartergrid, horiw$star_infs, mean)
+horic_1gr <- tapply(horiw$X1grid, horiw$star_infs, mean)
 
 # calculate mean of selected column (total land area of grid cells of occurrence) grouped by stars
-horia_mgr <- tapply(horiw$mgr_totalland, horiw$star_geo, mean)
-horia_qgr <- tapply(horiw$qgr_totalland, horiw$star_geo, mean)
-horia_1gr <- tapply(horiw$X1gr_totalland, horiw$star_geo, mean)
+horia_mgr <- tapply(horiw$mgr_totalland, horiw$star_infs, mean)
+horia_qgr <- tapply(horiw$qgr_totalland, horiw$star_infs, mean)
+horia_1gr <- tapply(horiw$X1gr_totalland, horiw$star_infs, mean)
 
 # merge resulting mean tables to one
 meanhori_count <- rbind(horic_mgr,horic_qgr,horic_1gr)
