f104cb5db16564fbc21421d5e399cc6bf4016bdd
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-03-15 10:47:31 +0000

    Added conversion of results to dataframe, and corrected write.csv script.

--- bin/examine_star_scenarios_for_sensitivity.R
+++ bin/examine_star_scenarios_for_sensitivity.R
@@ -17,14 +17,14 @@ star_columns <- c('star', 'star_geo', 'star_geod', 'star_geou', 'star_infa', 'st
 # stars_table <- rbind(star_table_scenario,,,,,)
 
 # individual scripts run in R
-splist_star <- filter_rows_by_valid_star(mainsplistR,'star')
-splist_star_geo <- filter_rows_by_valid_star(mainsplistR,'star_geo')
+splist_star      <- filter_rows_by_valid_star(mainsplistR,'star')
+splist_star_geo  <- filter_rows_by_valid_star(mainsplistR,'star_geo')
 splist_star_geou <- filter_rows_by_valid_star(mainsplistR,'star_geou')
 splist_star_geod <- filter_rows_by_valid_star(mainsplistR,'star_geod')
 splist_star_infa <- filter_rows_by_valid_star(mainsplistR,'star_infa')
 splist_star_infs <- filter_rows_by_valid_star(mainsplistR,'star_infs')
-star_table_star <- table(splist_star[,'star'])
-star_table_star_geo <- table(splist_star_geo[,'star_geo'])
+star_table_star      <- table(splist_star[,'star'])
+star_table_star_geo  <- table(splist_star_geo[,'star_geo'])
 star_table_star_geou <- table(splist_star_geou[,'star_geou'])
 star_table_star_geod <- table(splist_star_geod[,'star_geod'])
 star_table_star_infa <- table(splist_star_infa[,'star_infa'])
@@ -32,24 +32,44 @@ star_table_star_infs <- table(splist_star_infs[,'star_infs'])
 # summary table of star species numbers in all classification method, row=method by column=star. 
 # sum_table <- rbind(star_table_star,star_table_star_geo,star_table_star_geou,star_table_star_geod,star_table_star_infa,star_table_star_infs)
 # summary table of star species numbers in all classification method, row=star by column=method.
-sum_table <- cbind(star_table_star,star_table_star_geo,star_table_star_geou,star_table_star_geod,star_table_star_infa,star_table_star_infs)
+sum_table  <- cbind(star_table_star,star_table_star_geo,star_table_star_geou,star_table_star_geod,star_table_star_infa,star_table_star_infs)
 
 # calculate the differences of each star method from the default
 difference <- sum_table[,-1]-replicate(5,sum_table[,1])
 
-# bar plot
-barplot(sum_table,legend.text=c("star","star_geo","star_geou","star_geod","star_infa","star_infs"),beside=TRUE)
+
+# convert summary table and differences to dataframe
+sum_table_frame <- data.frame(star=row.names(sum_table), 			     
+		     star_default=sum_table[,1], 
+		     star_geo=sum_table[,2], 
+                     star_geou=sum_table[,3],
+                     star_geod=sum_table[,4], 
+                     star_infa=sum_table[,5], 
+                     star_infs=sum_table[,6],
+                     row.names=NULL)
+
+difference_frame <- data.frame(star=row.names(difference), 			 
+		     star_geo=difference[,1], 
+                     star_geou=difference[,2],
+                     star_geod=difference[,3], 
+                     star_infa=difference[,4], 
+                     star_infs=difference[,5],
+                     row.names=NULL)
  
 # write results to csv file. change sum_table to dataframe or modify the first row label 'star'.
-write.csv(sum_table,
+write.csv(sum_table_frame,
 	  file="../data/star_sensitivity_spnos_data.csv",
+          row.names=FALSE,
           fileEncoding="UTF-8")
 
-write.csv(difference,
+write.csv(difference_frame,
 	  file="../data/star_sensitivity_spnos_diff.csv",
+          row.names=FALSE,
           fileEncoding="UTF-8")
 
 # graphs needed: proportion bar graphs, one grouped by star on x; series = star_scenario, the other grouped by star_scenario; series = star
+# bar plot
+# barplot(sum_table,legend.text=c("star","star_geo","star_geou","star_geod","star_infa","star_infs"),beside=TRUE)
 ---------
 # alternative way?
 # extract the 'star' column of a chosen dataset
