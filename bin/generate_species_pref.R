78982d9a11e406f7e749d4d19187670d42f9f583
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-03-15 12:13:30 +0000

    Modified column name from x.Pref_en to Pref, according to changes in cell_pref.csv.

--- bin/generate_species_pref.R
+++ bin/generate_species_pref.R
@@ -3,20 +3,20 @@ species_cell <- read.csv('../data/species_cell.csv', row.names = NULL)
 cell_pref    <- read.csv('../data/cell_pref.csv',    row.names = NULL)
 
 # filter for relevant columns
-cell_pref <- cell_pref[,c('X.Pref_en','sampname')]
+cell_pref <- cell_pref[,c('Pref','sampname')]
 species_cell <- species_cell[,c('sampname','species','spnumber','star')]
 
 # merge dataframes
 species_pref <- merge(species_cell, cell_pref)
 
 # select unique species-prefecture pairs
-species_pref <- unique(species_pref[,c('species','spnumber','star','X.Pref_en')])
+species_pref <- unique(species_pref[,c('species','spnumber','star','Pref')])
 
 # remove subcells representing water parts of cells, with no prefecture
-species_pref <- subset(species_pref, X.Pref_en != "")
+species_pref <- subset(species_pref, Pref != "")
 
-# rename column from 'X.Pref_en' to 'pref'
-column_index <- which(names(species_pref) == 'X.Pref_en')
+# rename column from 'Pref' to 'pref'
+column_index <- which(names(species_pref) == 'Pref')
 names(species_pref)[column_index] <- 'pref'
 
 # write results to file
