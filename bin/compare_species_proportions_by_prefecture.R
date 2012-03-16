aaa432b791e4bf93ffe45bb5374502e457b8e2da
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-03-16 17:58:32 +0000

    Added cbind and write.csv of results.

--- bin/compare_species_proportions_by_prefecture.R
+++ bin/compare_species_proportions_by_prefecture.R
@@ -15,7 +15,19 @@ foj_table  <- table(foj[,c('star', 'pref')])
 
 # number of species by prefecture
 hori_spno <- table(hori[,c('pref')])
-foj_spno <- table(foj[,c('pref')])
+foj_spno  <- table(foj[,c('pref')])
+
+# merge species number into one table
+spnos <- cbind(foj_spno,hori_spno)
+
+# convert scores to dataframe
+spnos_frame <- data.frame(preflist=row.names(spnos), foj_spno=spnos[,1], hori_spno=spnos[,2], row.names=NULL)
+
+# write results (scores) to file
+write.csv(spnos_frame,
+          file="../data/FOJHori_spnos.csv",
+          row.names=FALSE,
+          fileEncoding="UTF-8")
 
 # paired-t test between the number of species per prefecture (or rather proportions relative to the total species pool) between hori and foj? check histogram if normally distributed. also think how to treat data = 0
 # rank correlation analysis for the sum number of species
@@ -25,3 +37,5 @@ cor(foj_spno,hori_spno, method = "spearman")
 compare_spnos <- lm(rank(t(hori_spno)) ~ rank(t(foj_spno)))
 
 # graphs to be considered.
+
+
