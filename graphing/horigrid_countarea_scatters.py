62dda193b7bbf9762358e9c4f21b2abd01ba5c25
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-04-02 11:57:49 +0100

    Removed "" from text string. Then, added colour coding of records on graph.

--- graphing/horigrid_countarea_scatters.py
+++ graphing/horigrid_countarea_scatters.py
@@ -1,43 +1,41 @@
 #!/usr/local/bin/ipython -i
 """
 A scatter graph of grid count vs grid area.
-Check for xy limits, possible in multi panel?
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
 # extract data from csv
+# CAUTION! column locations differ from filtered.
 # file_name = "../data/Hori_area_weight.csv"
 
 # uncomment when using filtered data
 file_name = "../data/Hori_area_weight_filtered.csv"
 
 # columns:
-# 12 - star_infs
-# 15, 13, 14 = horimgrid, quartergrid, X1grid [error in 13]
-# 19 - mgr_totalland
-# 23 - qgr_totalland
-# 26 - X1gr_totalland
-star_infs = np.genfromtxt(file_name, delimiter=',', dtype=None, skip_header=1, usecols=12)
-grid_count = np.genfromtxt(file_name, delimiter=',', dtype=None, skip_header=1, usecols=15)
-grid_land = np.genfromtxt(file_name, delimiter=',', dtype=None, skip_header=1, usecols=19)
+# 11 - star_infs
+# 14, 12, 13 = horimgrid, quartergrid, X1grid
+# 18 - mgr_totalland
+# 22 - qgr_totalland
+# 25 - X1gr_totalland
+star_infs = np.genfromtxt(file_name, delimiter=',', dtype=None, skip_header=1, usecols=11)
+grid_count = np.genfromtxt(file_name, delimiter=',', dtype=None, skip_header=1, usecols=14)
+grid_land = np.genfromtxt(file_name, delimiter=',', dtype=None, skip_header=1, usecols=18)
+# remove "" from the text string
+stars = [star[1:-1] for star in star_infs]
 
-
-# old script using a differently formatted dataset
-# spnos = np.array(list(np.genfromtxt("../data/spnos.csv",delimiter=',', dtype=None, names=True)[0])[1:])
-
-# threshold = 1500
 # colours = star_infs???
 # bk = grid_land[star_infs == "BK"]
 # gd = grid_land[star_infs == "GD"]
 # bu = grid_land[star_infs == "BU"]
 # gn = grid_land[star_infs == "GN"]
 # color=['k', 'y', 'b', 'g']
-# colours = map(lambda spno: 'r' if spno < threshold else 'b', spnos)
+
+colours = map(lambda star_colour: 'k' if star_colour == 'BK' else 'y' if star_colour == 'GD' else 'b' if star_colour == 'BU' else 'g' if star_colour == 'GN' else 'w', stars)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(grid_land, grid_count, alpha=0.5)
+ax.scatter(grid_land, grid_count, c=colours, alpha=0.5)
 
 #error: name xticks|yticks not defined.
 #xtix = arange(0, 380000.1, 500)
