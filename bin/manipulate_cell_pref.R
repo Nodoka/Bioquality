c96cf5be848c2302af3e0ae31a396b9faa0f7e0c
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-04-06 16:16:22 +0100

    Write.csv results. Deleted redundant data clean-ups done in GHI calculation.

--- bin/manipulate_cell_pref.R
+++ bin/manipulate_cell_pref.R
@@ -1,21 +1,12 @@
-source('../library/sanitise_data.R')
-
 # load data
 cell_pref  <- read.csv('../data/cell_pref.csv',    row.names = NULL)
 
-# OPTIONAL
-# rename column from 'Pref' to 'pref'
-column_index <- which(names(cell_pref) == 'Pref')
-		names(cell_pref)[column_index] <- 'pref'
-# set pref levels
-cell_pref <- set_pref_levels(cell_pref)
-
 # OPTIONAL: not necessary with a dataset after March 2012
 # remove subcells representing water parts of cells, with no prefecture
-cell_pref <- subset(cell_pref, pref != "")
+# cell_pref <- subset(cell_pref, pref != "")
 
 # unique pair of cell vs. pref
-cell_pref <- aggregate(cell_pref$sect_area, list(sampname=cell_pref$sampname,pref=cell_pref$Pref), sum)
+cell_pref <- aggregate(cell_pref$sect_area, list(sampname=cell_pref$sampname,Pref=cell_pref$Pref), sum)
 
 # rename the column "x"
 names(cell_pref)[3] <- 'sect_areasum'
@@ -23,6 +14,11 @@ names(cell_pref)[3] <- 'sect_areasum'
 # rearrange data in descending order of sect_areasum
 cell_pref <- cell_pref[order(-cell_pref$sect_area),]
 
-# order remaining, dont filter
 # select unique cell-pref pair
 cell_pref <- cell_pref[!duplicated(cell_pref$sampname),]
+
+# write results of cell pref unique pair
+write.csv(cell_pref,
+          file="../data/cell_uniqpref.csv",
+          row.names=FALSE,
+          fileEncoding="UTF-8")
