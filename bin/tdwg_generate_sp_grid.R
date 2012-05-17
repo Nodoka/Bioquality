13e2842cf1c05b87e513212bcf9f8dc53ea39238
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-05-17 15:25:38 +0100

    Added x1count paring, stats and weights for x1count.

--- bin/tdwg_generate_sp_grid.R
+++ bin/tdwg_generate_sp_grid.R
@@ -18,14 +18,41 @@ tdwg      <- tdwg[,c('spnumber','family','genus','sp1','rank1','sp2','star_infs'
 column_index <- which(names(tdwg_grid) == 'LEVEL3_COD') 
                 names(tdwg_grid)[column_index] <- 'l3code'
 
+# unique pair of grid vs. l3code
+grid_l3 <- aggregate(tdwg_grid$AREA, list(l3code=tdwg_grid$l3code,ID=tdwg_grid$ID), sum)
+
+# rename the column "x"
+names(grid_l3)[3] <- 'areas'
+
+# filter AREA < 0.01
+valid_area <- subset(grid_l3, areas > 0.009999)
+tdwg_grid <- valid_area
+
+# grid stats: the number of x1grid per l3code
+countsum <- table(tdwg_grid$l3code)
+
+# grid stats: mean and sum of x1 grid area per l3 code
+areamean <- tapply(tdwg_grid$areas, tdwg_grid$l3code, mean)
+areasum  <- tapply(tdwg_grid$areas, tdwg_grid$l3code, sum)
+
+# convert to dataframe
+gridstats <- data.frame(l3code=row.names(countsum), 
+                        x1sumcount=countsum[],
+                        x1areamean=areamean[],
+                        x1areasum=areasum[])
+
 # create species - grid list
 sp_grid <- merge(sp_tdwg, tdwg_grid)
 
+# rearrange data in descending order of sect_areasum
+# sp_grid <- sp_grid[order(-sp_grid$areas),]
+
 # select unique species-grid pairs for ID
-species_grid <- unique(sp_grid[,c('spnumber','star_infs','ID')])
+# species_grid <- sp_grid[!duplicated(sp_grid$ID),]
+# species_grid <- unique(sp_grid[,c('spnumber','star_infs','ID')])
 
 # grid count calculation
-sp_gridcount  <- table(species_grid[,'spnumber'])
+sp_gridcount  <- table(sp_grid[,'spnumber'])
 species_gridcount <- data.frame(spnumber=row.names(sp_gridcount),x1count=sp_gridcount[])
 
 # merge grid count and species list
