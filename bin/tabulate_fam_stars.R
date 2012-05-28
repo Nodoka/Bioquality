7db42f0cf347699d2a42b68cfa41f4ff032d6a19
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-05-28 13:36:44 +0100

    Make summary table of family scores and species no for main species list. Ignore csv file generated as a result.

---
+++ bin/tabulate_fam_stars.R
@@ -0,0 +1,38 @@
+source('../library/sanitise_data.R')
+source('../library/GHI.R')
+
+# load data for star sensitivity analysis
+mainsplistR   <- read.csv('../data/mainsplistR.csv',     row.names = NULL)
+
+# filter rows on star_infs
+main_starinfs <- filter_rows_by_valid_star(mainsplistR,'star_infs')
+
+# exclude Moss families?
+
+# caluclate family scores
+fam_scores <- tapply(main_starinfs$star_infs, main_starinfs$family, calculate_score)
+
+# summary table of star_infs by family
+fam_starinfs <- table(main_starinfs$family,main_starinfs$star_infs)
+# number of species per family
+spno_fam <- table(main_starinfs$family)
+# combine tables
+famstats <- cbind(fam_starinfs, spno_fam)
+
+# convert summary table and family socres to dataframe
+famstats_frame <- data.frame(family=row.names(famstats), 
+                    BK=famstats[,1],
+                    GD=famstats[,2],
+                    BU=famstats[,3], 
+                    GN=famstats[,4],
+                    spno=famstats[,5], 
+                    famscore=fam_scores[],row.names=NULL)
+
+# filter families with small number of species??
+
+# write results to csv
+write.csv(famstats_frame,
+          file="../data/mainsp_famstats.csv",
+          row.names=FALSE,
+          fileEncoding="UTF-8")
+
