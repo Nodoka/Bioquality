81d9418c45c4b8bd0be709801836cadfde931a15
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-06-06 11:42:28 +0100

    Stats test for FOJ vs Hori added.

--- bin/analyse_prefscores_agglomeration.R
+++ bin/analyse_prefscores_agglomeration.R
@@ -2,6 +2,27 @@
 brepeat <- read.csv('../data/prefscores_calc_methods.csv', row.names=NULL)
 bunique <- read.csv('../data/prefscores_calc_uniqmethods.csv', row.names=NULL)
 
+# the Shapiro-Wilk test of normality: shapiro.test(x)
+# summary statsitics: summary(x)
+# standard deviation: sapply(x, sd)
+
+## FOJ vs. Horikawa data
+# Spearman's rank correlation analysis:
+cor(brepeat$foj_scores, brepeat$hori_scores, method ='spearman')
+cor(brepeat$foj_scores, brepeat$meanGHI,     method ='spearman')
+cor(brepeat$foj_scores, brepeat$maxGHI,      method ='spearman')
+cor(bunique$foj_scores, bunique$hori_scores, method ='spearman')
+cor(bunique$foj_scores, bunique$meanGHI,     method ='spearman')
+cor(bunique$foj_scores, bunique$maxGHI,      method ='spearman')
+# Wilcoxon's signed-t test
+wilcox.test(brepeat$foj_scores, brepeat$hori_scores, paired=T)
+wilcox.test(brepeat$foj_scores, brepeat$meanGHI,     paired=T)
+wilcox.test(brepeat$foj_scores, brepeat$maxGHI,      paired=T)
+wilcox.test(bunique$foj_scores, bunique$hori_scores, paired=T)
+wilcox.test(bunique$foj_scores, bunique$meanGHI,     paired=T)
+wilcox.test(bunique$foj_scores, bunique$maxGHI,      paired=T)
+
+## Horikawa data
 # Spearman's rank correlation analysis: 
 # Brepeat vs Bunique on scores and spno
 cor(brepeat$hori_scores, bunique$hori_scores, method='spearman')
@@ -10,7 +31,6 @@ cor(brepeat$maxGHI,      bunique$maxGHI,      method='spearman')
 cor(brepeat$hori_spno,   bunique$hori_spno,   method='spearman')
 cor(brepeat$meanspno,    bunique$meanspno,    method='spearman')
 cor(brepeat$maxspno,     bunique$maxspno,     method='spearman')
-
 # Spearman's rank correlation analysis: 
 # Holistics vs mean, max for repeat and unique
 cor(brepeat$hori_scores, brepeat$maxGHI, method='spearman')
@@ -25,21 +45,16 @@ names(brepeat)[8] <- 'holsmaxGHI'
 names(bunique)[3] <- 'uniqhori_scores'
 names(bunique)[6] <- 'uniqmeanGHI'
 names(bunique)[8] <- 'uniqmaxGHI'
-
 # filter relevant columns & merge 2 data sets
 brep <- brepeat[c('preflist','holshori_scores','holsmeanGHI','holsmaxGHI')]
 bunq <- bunique[c('preflist','uniqhori_scores','uniqmeanGHI','uniqmaxGHI')]
 calscores <- merge(brep, bunq)
 
-# the Shapiro-Wilk test of normality
-# shapiro.test(x)
-
 # Wilcoxon's signed-t test:
 # Brepeat vs Bunique on scores
 wilcox.test(calscores$holshori_scores, calscores$uniqhori_scores, paired=T)
 wilcox.test(calscores$holsmeanGHI, calscores$uniqmeanGHI,     paired=T)
-wilcox.test(calscores$holsmaxGHI,      calscores$uniqmaxGHI,      paired=T)
-## 0 difference in uniq, max between the methods
+wilcox.test(calscores$holsmaxGHI,  calscores$uniqmaxGHI,      paired=T)
 
 # Wilcoxons' signed-t test: 
 # Holistics vs mean, max for repeat and unique
