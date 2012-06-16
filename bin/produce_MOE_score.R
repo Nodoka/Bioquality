90a8d575158ba652566bc21705ba7a720cfaffc9
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-06-16 11:57:50 +0100

    Added gazgird = latlong rounded at 3 decimal places.

--- bin/produce_MOE_score.R
+++ bin/produce_MOE_score.R
@@ -21,41 +21,52 @@ me_sp <- filter_rows_by_valid_star(me_sp,'star_infs')
 
 # select relevant columns
 me_plot <- valid_plot[,c('scode','sampname','lat','long')]
-me_sp <- me_sp[,c('scode','sampname','spnumber','star_infs')]
+me_sp   <- me_sp[,c('scode','sampname','spnumber','star_infs')]
 
 ## generate new sample plot unit  
 # select unique lat-long pair & name gazcode
 latlong <- unique(me_plot[,c('lat','long')])
 latlong$gazcode <- 1:nrow(latlong)  
 
-## gazgrid = lat/long at 4 decimal places??
+## gazgrid = lat/long rounded at 3 decimal places
+me_plot[,'rounded_lat']  <- round(me_plot[,'lat'],3)
+me_plot[,'rounded_long'] <- round(me_plot[,'long'],3)
+latlong <- unique(me_plot[,c('rounded_lat','rounded_long')])
+latlong$gazgrid <- 1:nrow(latlong)
 
 # add gazcode column based on unique lat/long combination
-plot <- merge(me_plot, latlong, by=c('lat','long'))
+plot   <- merge(me_plot, latlong) # by=c('lat','long')
 
 # merge gazcode unit based on scode 
 splist <- merge(me_sp, plot)
 
 # unique sort species using spnumber 
 me_sp <- unique(splist[,c('scode','sampname','spnumber','star_infs','lat','long','gazcode')])
+me_sp <- unique(splist[,c('scode','sampname','spnumber','star_infs','lat','long','gazgrid')])
 
 # GHI grouped by plot unit for selected star method
-me_score <- tapply(me_sp$star_infs,  me_sp$scode, calculate_score)
+me_score <- tapply(me_sp$star_infs,  me_sp$scode,    calculate_score)
 me_score <- tapply(me_sp$star_infs,  me_sp$gazcode,  calculate_score)
+me_score <- tapply(me_sp$star_infs,  me_sp$gazgrid,  calculate_score)
 
 # calculate spcount for plot
 spno <- table(me_sp$scode)
 spno <- table(me_sp$gazcode)
+spno <- table(me_sp$gazgrid)
 
 # transfer results (score and spcount) to plot data
-plot_score <- cbind(me_score, spno)
+plot_score      <- cbind(me_score, spno)
 plotscore_frame <- data.frame(gazcode=row.names(plot_score),
+                             GHI=plot_score[,1],
+                             spno=plot_score[,2])
+
+plotscore_frame <- data.frame(gazgrid=row.names(plot_score),
                              GHI=plot_score[,1],
                              spno=plot_score[,2])
 
 gazscores <- merge(latlong, plotscore_frame, all.x=T)
 
-# make a new column with either of cell or pref score absed on spno
+# make a new column with valid score based on spno
 less_than_40 <- gazscores$spno < 40
 less_than_40[is.na(less_than_40)] <- T
 
@@ -77,3 +88,12 @@ write.csv(valid_gazscores,
           row.names=FALSE,
           fileEncoding="UTF-8")
 
+write.csv(gazscores,
+          file="../data/MOEgazgrid_scoreR.csv",
+          row.names=FALSE,
+          fileEncoding="UTF-8")
+
+write.csv(valid_gazscores,
+          file="../data/MOEgazgrid_validscoreR.csv",
+          row.names=FALSE,
+          fileEncoding="UTF-8")
