8566005b738b9f6b5efb7e9be6c25ba1e46fecdd
Nodoka Nakamura <nodoka.nakamura@gmail.com>
2012-08-08 12:05:03 +0100

    Adjusted column numbers for the new file.

--- graphing/utaki_hori_scatter.py
+++ graphing/utaki_hori_scatter.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 # extract utaki data
 utaki_file = "../data/Utakigaz_Horim.csv"
 # use columns
-# 4 = gazcode (32 - 517)
-# 11 = spcount as 'taxa'
-# 22 = GHI40 for Utakigaz as 'validscore'
-# 37 = XMGHI40
-gazcode = np.genfromtxt(utaki_file, delimiter=',', dtype=None, skip_header=1, usecols=4)
-utaki_score = np.genfromtxt(utaki_file, delimiter=',', dtype=None, skip_header=1, usecols=22)
-utakihori_score = np.genfromtxt(utaki_file, delimiter=',', dtype=None, skip_header=1, usecols=37)
-utakispcount = np.genfromtxt(utaki_file, delimiter=',', dtype=None, skip_header=1, usecols=11)
+# 0 = gazcode (32 - 517) [previously 4]
+# 9 = spcount as 'taxa' [11]
+# 10 = GHI40 for Utakigaz as 'validscore' [22]
+# 17 = XMGHI40 [37]
+gazcode = np.genfromtxt(utaki_file, delimiter=',', dtype=None, skip_header=1, usecols=0)
+utaki_score = np.genfromtxt(utaki_file, delimiter=',', dtype=None, skip_header=1, usecols=10)
+utakihori_score = np.genfromtxt(utaki_file, delimiter=',', dtype=None, skip_header=1, usecols=17)
+utakispcount = np.genfromtxt(utaki_file, delimiter=',', dtype=None, skip_header=1, usecols=9)
 
 # add y = x line
 #lx = np.arange(0,460)
@@ -34,8 +34,8 @@ ax = fig.add_subplot(111)
 #ax.plot(lx, ly, c='k', alpha=0.2) 
 ax.scatter(utakihori_score, utaki_score, s=utaki_size, color = colours, marker='d', alpha=0.5)
 ax.set_aspect(1) 
-ax.set_xlim([0, 560])
-ax.set_ylim([0, 560])
+ax.set_xlim([0, 500])
+ax.set_ylim([0, 500])
 ax.set_xlabel('Horikawa score', fontsize=16)
 ax.set_ylabel('Utaki grove score', fontsize=16)
 ax.grid(True)
